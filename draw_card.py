731daaba51cdad9f6ac4ef60a00db1c67b403de0
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-03-06 10:35:15 -0500

    prototype CLI tarot draw basic command loop working needs formatting + expanded JSON

--- draw_card.py
+++ draw_card.py
@@ -1,12 +1,106 @@
 import json
 import random
+import time
+import readline
+
+
+boot_screen = [
+    "__________________________________________________________________",
+    " ",
+    "                          M O I R E   T A R O T   C A R D  P U L L",
+    "                                            ==  prototype v0.1 == ",
+    "__________________________________________________________________",
+    " ",
+    " A SILLYLIL APP   BY: S.D.K. MOIRE 'AUBEL'",
+    "__________________________________________________________________",
+    " ",
+    "===================================================================",
+    "===================================================================",
+    " ",
+    " ",
+    "            ███╗   ███╗ ██████╗ ██╗██████╗ ███████╗    ",
+    "            ████╗ ████║██╔═══██╗██║██╔══██╗██╔════╝    ",
+    "            ██╔████╔██║██║   ██║██║██████╔╝█████╗      ",
+    "            ██║╚██╔╝██║██║   ██║██║██╔══██╗██╔══╝      ",
+    "            ██║ ╚═╝ ██║╚██████╔╝██║██║  ██║███████╗    ",
+    "            ╚═╝     ╚═╝ ╚═════╝ ╚═╝╚═╝  ╚═╝╚══════╝    ",
+    "                                                       ",
+    "            ████████╗ █████╗ ██████╗  ██████╗ ████████╗",
+    "            ╚══██╔══╝██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝",
+    "               ██║   ███████║██████╔╝██║   ██║   ██║   ",
+    "               ██║   ██╔══██║██╔══██╗██║   ██║   ██║   ",
+    "               ██║   ██║  ██║██║  ██║╚██████╔╝   ██║   ",
+    "               ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ",
+    " ",
+    " ",
+    "===================================================================",
+    "===================================================================",
+    " ",
+    "   Software Brought to You by  CHESTER'S IMPORTS ",
+    "   'Quality provider of unique imports, including you!' ",
+    "   copyright '3639 all rights reserved ",
+]
+
+
+class s:
+    r = '\033[91m'
+    sHg = '\033[102m'
+    sHb = '\033[1;33;44m'
+    sHy = '\033[103m'
+
+    sHr = '\033[101m'
+    sHYr = '\033[1;31;43m'
+
+    g = '\033[1;92m'
+    ga = '\033[1;30;49m'
+    HWr = '\033[0;31;47m'
+    b = '\033[94m'
+    y = '\033[93m'
+    sU = '\033[4m'
+    sB = "\033[2m"
+    x = '\033[00m'
 
 with open("major_arcana.json") as f:
     cards = json.load(f)
 
-card = random.choice(cards)
+for line in boot_screen:
+    print(line)
+    time.sleep(0.05)
 
-print(card["number"])
-print(card["name"])
-print(card["definition"])
-print(card["symbols"])
+print(f"""__________________________________________________________________
+
+ Welcome to MOIRE TAROT. Well.. it is more of an oracle deck, it
+ we're being perfectly honest. And honesty is the best policy,
+ after all. {s.y}Or is it?{s.x}
+
+ Who taught you that anyway?
+
+ Ahem. NOW.........
+""", flush=True)
+
+daily_card = random.choice(cards)
+
+
+def run():
+    print(f"""
+    WEEEEEEEEEEEELCOME *mysterious voice*
+    I see some kind of tarot in your future.
+
+type {s.g}DRAW{s.x} to draw a card. 
+type {s.g}END{s.x} to terminate this experience.
+    """)
+
+
+    while True:
+        user_input = input(f"{s.sHYr} Command > {s.x} ")
+
+        if user_input.lower() == "end":
+            print("The wolf dissolves back into the wires.")
+            break
+
+        if user_input.lower() == "draw":
+            card = random.choice(cards)
+            print(card["ig_name"])
+
+if __name__ == "__main__":
+    run()
